25d6d4f282f1417995f686c515ce842c01b9b6bc
malis42 <mateuuszlis@gmail.com>
2020-08-26 11:34:44 +0200

    Refactored main.js so code is reusable for different leagues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,57 @@
-const countdown = document.querySelector('.countdown');
+// const countdown = document.querySelector('.countdown');
 
-// Set launch date [ms]
-const launchDate = new Date('Sep 12, 2020 16:00:00 GMT+1').getTime();
+// Set launch dates [ms]
+const premierLeagueLaunchDate = new Date('Sep 12, 2020 16:00:00 GMT+1').getTime();
+const laLigaLaunchDate = new Date('Sep 12, 2020 20:00:00 GMT').getTime();
+const bundesligaLaunchDate = new Date('Sep 18, 2020 20:30:00 GMT').getTime();
 
-// Update countdown every second
-const PLinterval = setInterval(() => {
-    // Get today's date and time in ms
-    const now = new Date().getTime();
+// Set league names
+const premierLeagueName = 'premier-league';
+const laLigaName = 'la-liga';
+const bundesligaName = 'bundesliga';
 
-    // Get time difference from now to launch date
-    const distance = launchDate - now;
 
-    // Time calculations
-    const days = Math.floor(distance / (1000*60*60*24));
-    const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-    const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-    const seconds = Math.floor((distance % (1000*60)) / (1000));
+function initCountdown(leagueName, launchDate) {
+    // Get element by league name id
+    const countdownDiv  = document.getElementById(`${leagueName}-countdown`);
 
-    // console.log(days, hours, minutes, seconds)
+    // Update countdown every second
+    const interval = setInterval(() => {
+        // Get today's date and time in ms
+        const now = new Date().getTime();
 
-    // Display the result
-    countdown.innerHTML = `
+        // Get time difference from now to launch date
+        const distance = launchDate - now;
+
+        // Time calculations
+        const days = Math.floor(distance / (1000*60*60*24));
+        const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+        const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+        const seconds = Math.floor((distance % (1000*60)) / (1000));
+
+        // console.log(days, hours, minutes, seconds)
+
+        // Display the result
+        countdownDiv.innerHTML = `
         <div>${days}<span>Days</span></div>
         <div>${hours}<span>Hours</span></div>
         <div>${minutes}<span>Minutes</span></div>
         <div>${seconds}<span>Seconds</span></div>
     `;
 
-    // If launchdate passed
-    if(distance < 0){
-        // Stop countdown
-        clearInterval(PLinterval);
+        // If launchdate passed
+        if(distance < 0){
+            // Stop countdown
+            clearInterval(interval);
 
-        // Style and output text
-        countdown.style.color = '#17a2b8';
-        countdown.innerHTML = 'Premier League has started!'
-    }
-}, 1000);
+            // Style and output text
+            countdownDiv.style.color = '#17a2b8';
+            countdownDiv.innerHTML = 'Premier League has started!'
+        }
+    }, 1000);
+}
+
+// Init countdowns for every league
+initCountdown(premierLeagueName, premierLeagueLaunchDate);
+initCountdown(laLigaName, laLigaLaunchDate);
+initCountdown(bundesligaName, bundesligaLaunchDate);
