489a1513c64c6978c8c4bbc6e9e3254971cf926d
malis42 <mateuuszlis@gmail.com>
2020-08-27 14:14:07 +0200

    fixing top navbar

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 
+console.log(tabs);
+
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
        const target = document.querySelector(tab.dataset.tabTarget);
